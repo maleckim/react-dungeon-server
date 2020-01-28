b4a9fb40ffcbcd3a507acca8d9f5852ca797c822
maleckim <maleckimatthewm@gmail.com>
2020-01-27 19:16:47 -0600

    added stranger

--- Dialogue.js
+++ Dialogue.js
@@ -2,5 +2,12 @@ module.exports = [
   {
     who: 'Master',
     Responses:['Welcome', 'Yeah sure', 'k let\'s move on']
+  },
+
+  {
+    who: 'Stranger',
+    Responses:['Here ya go', 'prepare to die', 'not today weirdo']
   }
+
+
 ]
